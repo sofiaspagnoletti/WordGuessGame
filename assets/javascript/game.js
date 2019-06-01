51d59e6cfd4c634a544f187c53dba879b97b0856
sofi.spag <sofi.spag@gmail.com>
2019-05-31 18:35:13 -0700

    "fixing link to JavaScript"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,7 @@
-var allOptions = ["Sofia", "Andrew", "Joe", "Sveta", "Felix", "Helan", "Monali", "Devin"];
+
+
+var allWords = ["sofia", "andrew", "joe", "sveta", "felix", "helan", "monali", "devin"];
+var allLettersOption = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 var wins = 0;
 var losses = 0;
@@ -6,14 +9,30 @@ var numGuesses = 10;
 var letterUsed = [];
 
 function getRandomWord() {
-    var randomNumber = Math.floor(Math.random() * allOptions.length);
-    var randomWord = allOptions[randomNumber];
+    var randomNumber = Math.floor(Math.random() * allWords.length);
+    var randomWord = allWords[randomNumber];
     return randomWord;
 }
 
+function wordReplacement() {
+    randomWord = getRandomWord();
+    currentGuess = [];
+    for (var i = 0; i < randomWord.length; i++) {
+        currentGuess.push("_");
+    }
+}
+
 function isUserInputValid(userInput) {
-    // Checks if the input is part of the allOptions array
-    return allOptions.indexOf(userInput) > -1;
+    return allLettersOption.indexOf(userInput) > -1;
+}
+
+function isLetterPresentInOurWord(chosenLetter) {
+    return randomWord.includes(chosenLetter);
+}
+
+function allLettersArePresent() {
+    replaceChosenLetter=randomWord;
+    resetGame();
 }
 
 function win() {
@@ -28,8 +47,8 @@ function lose() {
 
 function resetGame() {
     numGuesses = 10;
-    guessChoices = [];
-    randomWord = getRandomWord();
+    letterUsed = [];
+    wordReplacement();
 }
 
 function writeGameStatus() {
@@ -39,7 +58,10 @@ function writeGameStatus() {
     document.querySelector('#letters-used').innerHTML = "Your missed letters so far: " + letterUsed;
 }
 
-var randomWord = getRandomWord();
+var randomWord = "";
+var currentGuess = [];
+
+wordReplacement();
 
 document.onkeyup = function (event) {
 
@@ -48,17 +70,28 @@ document.onkeyup = function (event) {
     // Only calculate if the user entered a valid key.
     if (isUserInputValid(userGuess)) {
 
-        if (userGuess === computerGuess) {
-            win();
+        if (isLetterPresentInOurWord(userGuess)) {
+            replaceChosenLetter();
+        } else {
+            numGuesses--;
+            letterUsed.push(userGuess);
         }
 
-        if (userGuess != computerGuess) {
-            numGuesses--;
-            guessChoices.push(userGuess);
-        }
+        // if (userGuess === computerGuess) {
+        //     win();
+        // }
+
+        // if (userGuess != computerGuess) {
+        //     numGuesses--;
+        //     guessChoices.push(userGuess);
+        // }
 
         if (numGuesses === 0) {
             lose();
+        }
+
+        if (allLettersArePresent()) {
+            win();
         }
 
         writeGameStatus();
